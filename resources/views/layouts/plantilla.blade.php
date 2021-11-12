7d5a28be8a0c5e19a434fd17649940eedc58e6ac
sebastian8080 <sebas25211@hotmail.com>
2021-11-12 17:10:48 -0500

    Modificando diseño de navbar responsive - Agregando fondo

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -19,11 +19,11 @@
     <nav id="menu" class="navbar navbar-expand-lg fixed-top navbar-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="{{ route('pages.home') }}"><img id="img-logo" src="{{ URL::asset('casacredito-logo.png'); }}" alt=""></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button id="btn-collapse-navbar" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul id="itemMenu" class="navbar-nav">
+            <ul id="itemMenu" class="navbar-nav rounded">
               <li class="nav-item">
                 <a id="menu-item1" class="nav-link text-white rounded" aria-current="page" href="{{ route('pages.about') }}">NOSOTROS</a>
               </li>
