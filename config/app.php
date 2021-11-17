2bee864bc376d935a0ae13c049557a2b401aaaef
sebastian8080 <sebas25211@hotmail.com>
2021-11-17 09:28:05 -0500

    Retirando service provider genera error

--- config/app.php
+++ config/app.php
@@ -173,8 +173,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        JeroenNoten\LaravelAdminLte\AdminLteServiceProvider::class
+        App\Providers\RouteServiceProvider::class
 
     ],
 
@@ -229,7 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'AdminLTE' => JeroenNoten\LaravelAdminLte\AdminLte::class
 
     ],
 
