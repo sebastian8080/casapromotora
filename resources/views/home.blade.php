3ab84b36f7a99906efb74418f35b8b58e6f8737f
sebastian8080 <sebas25211@hotmail.com>
2021-11-17 09:25:24 -0500

    Agregando adminlte service provider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,10 +12,30 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    
 @stop
 
 @section('js')
     <script>console.log('Hi!');</script>
 @stop
+{{-- @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Bienvenido') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Inicio de sesion correcto') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
