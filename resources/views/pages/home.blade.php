28f965197ef0a3896cfa716dd169fa580b9e924c
sebastian8080 <sebas25211@hotmail.com>
2022-01-04 15:05:27 -0500

    Modificando diseño del banner de projects en la pagina home -> no se visualiza bien en movil

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -83,21 +83,21 @@
             <div id="carouselExampleControls2" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img id="imgBanner1" src="{{ asset('img/projects/adra/1.webp') }}" class="d-block img-fluid" width="100%" alt="Proyecto Adra Casa Credito Promotora">
+                  <img id="imgBanner1" src="{{ asset('img/projects/adra/1.webp') }}" class="d-block img-fluid" width="100%" alt="Departamentos Adra - Cuenca Casa Credito Promotora">
                   <div class="carousel-caption d-md-block">
                     <a style="text-decoration: none" href="{{ route('projects.viewProject', 'Adra') }}"><h3 class="texto-imagenes">ADRA</h3></a>
                     <p>Departamentos de lujo con una vista insuperable</p>
                   </div>
                 </div>
                 <div class="carousel-item">
-                  <img id="imgBanner2" style="width: 100%" src="{{ asset('img/projects/futuranarancay/1.webp') }}" class="d-block img-fluid" width="100%" alt="Proyecto Futura Narancay Casa Credito Promotora">
+                  <img id="imgBanner2" src="{{ asset('img/projects/futuranarancay/1.webp') }}" class="d-block img-fluid" width="100%" alt="Departamentos Futura Narancay - Cuenca Casa Credito Promotora">
                   <div class="carousel-caption d-md-block">
                     <a style="text-decoration: none" href="{{ route('projects.viewProject', 'Futura Narancay') }}"><h3 class="texto-imagenes">FUTURA NARANCAY</h3></a>
                     <p>Departamentos con una  experiencia única en diseño y comodidad</p>
                   </div>
                 </div>
                 <div class="carousel-item">
-                  <img id="imgBanner3" src="{{ asset('img/projects/toscana/1.webp') }}" class="d-block img-fluid" width="100%" alt="...">
+                  <img id="imgBanner3" src="{{ asset('img/projects/toscana/1.webp') }}" class="d-block img-fluid" width="100%" alt="Condominios Toscana - Cuenca">
                   <div class="carousel-caption d-md-block">
                     <a style="text-decoration: none" href="{{ route('projects.viewProject', 'Toscana') }}"><h3 class="texto-imagenes">TOSCANA</h3></a>
                     <p>Condominios con acabados modernos</p>
