31bb89c4f9932d7fd32a0a8aa0c51c7d9afe548c
sebastian8080 <sebas25211@hotmail.com>
2022-04-13 15:40:47 -0500

    Agregando la meta etiqueta product availability

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -13,6 +13,8 @@
 <meta property="og:description" content="{!! $data['descripcion'] !!}">
 <meta property="og:image" content="{{url('img/projects/'.$data['name_folder'].'/1.webp')}}">
 
+<meta property="product:availability" content="in stock">
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <style>
       body{
