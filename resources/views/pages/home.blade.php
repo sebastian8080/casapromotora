c44c7638012909c75a219a2e9edceb505f4bccaa
sebastian8080 <sebas25211@hotmail.com>
2023-08-02 12:10:37 -0500

    quitando clase shadow

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -54,7 +54,7 @@
         <a href="{{ route('projects.viewProject', [strtolower($condominio->type), $condominio->slug]) }}">
           <article class="position-relative">
             <img  width="100%" height="350px" src="{{ asset('uploads/projects/300/' . strtok($condominio->images, '|')) }}" alt="{{ $condominio->type }} en Venta en Cuenca">
-            <p class="position-absolute top-0 start-0 bg-white text-dark px-3 py-1 text-center shadow" style="width: 70%; font-weight: 600">{{ strtoupper($condominio->project_name) }}</p>
+            <p class="position-absolute top-0 start-0 bg-white text-dark px-3 py-1 text-center" style="width: 70%; font-weight: 600">{{ strtoupper($condominio->project_name) }}</p>
           </article>
         </a>
       </div>
@@ -70,7 +70,7 @@
         <a href="{{ route('projects.viewProject', [strtolower($department->type), $department->slug]) }}">
           <article class="position-relative">
             <img  width="100%" height="350px" src="{{ asset('uploads/projects/300/' . strtok($department->images, '|')) }}" alt="{{ $department->type }} en Venta en Cuenca">
-            <p class="position-absolute @if($loop->index == 0)bottom-0 start-0 @elseif($loop->index == 1)bottom-0 start-50 translate-middle-x @elseif($loop->index == 2)bottom-0 end-0 @endif bg-white text-dark px-3 py-1 text-center shadow" style="width: 70%; margin-bottom:0px; font-weight: 600">{{ strtoupper($department->project_name) }}</p>
+            <p class="position-absolute @if($loop->index == 0)bottom-0 start-0 @elseif($loop->index == 1)bottom-0 start-50 translate-middle-x @elseif($loop->index == 2)bottom-0 end-0 @endif bg-white text-dark px-3 py-1 text-center" style="width: 70%; margin-bottom:0px; font-weight: 600">{{ strtoupper($department->project_name) }}</p>
           </article>
         </a>
       </div>
