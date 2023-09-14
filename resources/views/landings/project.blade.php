9f1d70f29fffd545f3db83af5e21f612becc61dc
sebastian8080 <sebas25211@hotmail.com>
2023-09-14 11:21:16 -0500

    agregando javascript en la vista blade de projects y no en el componente

--- resources/views/landings/project.blade.php
+++ resources/views/landings/project.blade.php
@@ -123,5 +123,30 @@
 
 <livewire:scripts />
 <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+<script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
+</script>
 </body>
 </html>
