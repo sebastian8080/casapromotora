4078f32f58c911668ffdd20e43973bab66633b5d
sebastian8080 <sebas25211@hotmail.com>
2023-10-24 16:28:31 -0500

    agregando meta etiqueta no index

--- resources/views/pages/thank.blade.php
+++ resources/views/pages/thank.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gracias por contactarse con Casa Promotora</title>
 
