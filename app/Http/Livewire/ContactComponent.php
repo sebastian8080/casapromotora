b338acf492fbeb87ed22b0296e6fa4240d958455
sebastian8080 <sebas25211@hotmail.com>
2024-02-29 14:46:03 -0500

    cambiando color de barra de navegacion

---
+++ app/Http/Livewire/ContactComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class ContactComponent extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.contact-component');
+    }
+}
